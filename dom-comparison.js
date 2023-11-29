ae969a0b2e53cdec215e8abe1a0de0b866566295
Raguru Sai Sandeep <62465620+SaiSandeepRaguru@users.noreply.github.com>
2023-11-29 21:50:06 +0530

    Update dom-comparison.js

--- dom-comparison.js
+++ dom-comparison.js
@@ -63,30 +63,25 @@ function generateDescriptions(changes) {
   
   function getDescription(element, changeType) {
     if (changeType === 'added') {
-      // Example: Checking for specific classes, IDs, or other attributes to describe the added element
       if (element.classList.contains('sidebar')) {
         return `Sidebar is present in the second DOM.`;
       } else if (element.id === 'close-button') {
         return `Close button is available in the second DOM.`;
       } else {
-        // Default to the tag name if no specific identifier found
         return `${element.tagName} is present in the second DOM.`;
       }
     } else if (changeType === 'removed') {
-      // Similar approach for removed elements
       if (element.classList.contains('sidebar')) {
         return `Sidebar is not present in the second DOM anymore.`;
       } else if (element.id === 'close-button') {
         return `Close button is not available in the second DOM anymore.`;
       } else {
-        // Default to the tag name if no specific identifier found
         return `${element.tagName} is not present in the second DOM anymore.`;
       }
     }
   }
   
 
-// Function to generate descriptive text for an element
 function getDescription(element, changeType) {
   if (changeType === 'added') {
     return `${element.tagName} is present in the second DOM.`;
@@ -100,26 +95,20 @@ async function compareAmazonHomePage() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   
-  // Navigate to Amazon's homepage
   await page.goto('https://www.amazon.com/');
   await page.waitForSelector('#nav-hamburger-menu');
 
-  // Capture HTML before clicking the hamburger menu
   const beforeHTML = await page.content();
 
-  // Simulate click on the hamburger menu
   await page.click('#nav-hamburger-menu');
 
-  // Wait for the changes to reflect
   await page.waitForTimeout(2000);
   
   // Capture HTML after clicking the hamburger menu
   const afterHTML = await page.content();
 
-  // Compare DOMs
   const changes = compareDOMs(beforeHTML, afterHTML);
 
-  // Generate descriptive sentences
   const descriptions = generateDescriptions(changes);
   
   console.log(descriptions.join('\n')); // Display the descriptions
